30bdc1ba9ee33a9cf301a11017c425c613abd9ad
nkrim62 <nkrim62@gmail.com>
2019-02-26 18:53:21 -0800

    Updated spritesheet to remove in/out power, and add the power module

--- aseprite_export.sh
+++ aseprite_export.sh
@@ -8,8 +8,8 @@ sprite_dir=./assets/sprites
 out_dir=./data/graphics
 
 # wire_sprite_64x64.aseprite
-sprite=$sprite_dir/wire_sprite_64x64.aseprite
-out=$out_dir/wire_sheet.png
+sprite=$sprite_dir/module_sprite_64x64.aseprite
+out=$out_dir/module_sheet.png
 $ase --batch \
 --layer "Tile" $sprite \
 --ignore-layer "Tile" \
@@ -19,25 +19,20 @@ $ase --batch \
 --ignore-layer "Center" \
 --layer "Up/Shadow" $sprite \
 --layer "Up/BG" --layer "Up/Default" $sprite \
---layer "Up/In" $sprite \
---layer "Up/Out" $sprite \
---ignore-layer "Up/In" $sprite \
+--layer "Up/In" --layer "Up/Out" $sprite \
 --ignore-layer "Up" \
 --layer "Down/Shadow" $sprite \
 --layer "Down/BG" --layer "Down/Default" $sprite \
---layer "Down/In" $sprite \
---layer "Down/Out" $sprite \
---ignore-layer "Down/In" $sprite \
+--layer "Down/In" --layer "Down/Out" $sprite \
 --ignore-layer "Down" \
 --layer "Left/Shadow" $sprite \
 --layer "Left/BG" --layer "Left/Default" $sprite \
---layer "Left/In" $sprite \
---layer "Left/Out" $sprite \
---ignore-layer "Left/In" $sprite \
+--layer "Left/In" --layer "Left/Out" $sprite \
 --ignore-layer "Left" \
 --layer "Right/Shadow" $sprite \
 --layer "Right/BG" --layer "Right/Default" $sprite \
---layer "Right/In" $sprite \
---layer "Right/Out" $sprite \
---ignore-layer "Right/In" $sprite \
+--layer "Right/In" --layer "Right/Out" $sprite \
+--ignore-layer "Right" \
+--layer "Power/BG" $sprite \
+--layer "Power/On" $sprite \
 --sheet $out --sheet-pack
